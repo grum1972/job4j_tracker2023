a85d11b6f4b5fd3153f6773ac286ec7f0540af97
Kotov Oleg <grum1972@gmail.com>
2022-12-23 21:24:26 +0300

    1.7. Конструктор [#173889 #197271]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error err = new Error();
+        Error err1 = new Error(true, 200, "Ok!");
+        err.printInfo();
+        err1.printInfo();
+    }
+}
