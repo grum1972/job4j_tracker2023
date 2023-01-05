1218eecc2124e578430da60ea0b82801a2acddc9
Kotov Oleg <grum1972@gmail.com>
2023-01-05 14:29:44 +0300

    1. Singleton [#94606 #201878]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+    private Tracker tracker = new Tracker();
+
+    private static SingleTracker instance = null;
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String name) {
+        return tracker.findByName(name);
+    }
+
+    public Item[] findAll(int id) {
+        return tracker.findAll();
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
