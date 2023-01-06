6c36c841a5115953ef77caa1b32df91d3cb62674
Kotov Oleg <grum1972@gmail.com>
2023-01-06 09:32:14 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #377750]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -3,47 +3,13 @@ package ru.job4j.early;
 import static java.lang.Character.*;
 
 public class PasswordValidator {
-    private static boolean hasDigit(String string) {
-        for (int i = 0; i < string.length(); i++) {
-            int symbol = string.charAt(i);
-            if (isDigit(symbol)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean hasLowerCaseLetter(String string) {
-        for (int i = 0; i < string.length(); i++) {
-            int symbol = string.charAt(i);
-            if (isLowerCase(symbol)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean hasUpperCaseLetter(String string) {
-        for (int i = 0; i < string.length(); i++) {
-            int symbol = string.charAt(i);
-            if (isUpperCase(symbol)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean hasSpecialSymbol(String string) {
-        for (int i = 0; i < string.length(); i++) {
-            int symbol = string.charAt(i);
-            if (!isLetter(symbol) && !isDigit(symbol)) {
-                return true;
-            }
-        }
-        return false;
-    }
 
     public static String validate(String password) {
+        boolean hasDigit = false;
+        boolean hasLowerCaseLetter = false;
+        boolean hasUpperCaseLetter = false;
+        boolean hasSpecialSymbol = false;
+
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
@@ -52,30 +18,47 @@ public class PasswordValidator {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
 
-        if (!hasUpperCaseLetter(password)) {
+        String[] words = {"qwerty", "12345", "password", "admin", "user"};
+        for (String word : words) {
+            if (password.toLowerCase().contains(word.toLowerCase())) {
+                throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+            }
+        }
+
+        for (int i = 0; i < password.length(); i++) {
+            int symbol = password.charAt(i);
+            if (isDigit(symbol)) {
+                hasDigit = true;
+                continue;
+            }
+            if (isLowerCase(symbol)) {
+                hasLowerCaseLetter = true;
+                continue;
+            }
+            if (isUpperCase(symbol)) {
+                hasUpperCaseLetter = true;
+                continue;
+            }
+            if (!isLetter(symbol) && !isDigit(symbol)) {
+                hasSpecialSymbol = true;
+            }
+        }
+
+        if (!hasUpperCaseLetter) {
             throw new IllegalArgumentException("Password should contain at least one uppercase letter");
         }
 
-        if (!hasLowerCaseLetter(password)) {
+        if (!hasLowerCaseLetter) {
             throw new IllegalArgumentException("Password should contain at least one lowercase letter");
         }
 
-        if (!hasDigit(password)) {
+        if (!hasDigit) {
             throw new IllegalArgumentException("Password should contain at least one figure");
         }
 
-        if (!hasSpecialSymbol(password)) {
+        if (!hasSpecialSymbol) {
             throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
-
-        if (password.toLowerCase().contains("qwerty".toLowerCase())
-                || password.toLowerCase().contains("12345".toLowerCase())
-                || password.toLowerCase().contains("password".toLowerCase())
-                || password.toLowerCase().contains("admin".toLowerCase())
-                || password.toLowerCase().contains("user".toLowerCase())) {
-            throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
-        }
-
         return password;
     }
 }
