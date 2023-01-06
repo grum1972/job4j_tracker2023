612285b098fda5a6d7f9fbaae528021609603aa5
Kotov Oleg <grum1972@gmail.com>
2023-01-06 12:02:56 +0300

    0. Map, HashMap [#212652 #204166]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("mail@mail.ru", "Ivanov");
+        map.put("mail1@mail.ru", "Petrov");
+        map.put("mail2@mail.ru", "Sidorov");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
